3491b7a5abd174948b508c8265ce3326da4bcbef
Joshuapavan <joshuapavan35@gmail.com>
2022-03-26 08:58:45 +0530

    Initial Setup for the Backend application.

---
+++ src/main/java/com/GRANDEUR/GrandeurBackend/GrandeurBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GRANDEUR.GrandeurBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandeurBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandeurBackendApplication.class, args);
+	}
+
+}
